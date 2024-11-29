9c2597fc8e822bb7efcabac2d07a29a4b74ef5b7
kunkun <iqbalsetiawan536@gmail.com>
2024-11-29 14:02:51 +0700

    baru

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -1,26 +1,72 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="main-content">
-        <div class="col-md-12 row">
-            <div class="card-wrap">
-                <div class="card-header">
-                  <h4>Total Orders</h4>
+@include('template.header')
+@include('template.navbar')
+@include('template.sidebar')
+<div class="main-content">
+    <section class="section">
+        <div class="section">
+            <div class="">
+                <div class="col-md-12 form-inline">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h1 class="form-inline navbar-brand" style="padding-left: 1%">Default Layout</h1>
+                            </div>
+                            <div class="col-md-6">
+                            </div>
+                            <div class="card-body p-0">
+                                <div class="section-body">
+                                    <div class="row">
+                                      <div class="col-12 col-md-6 col-lg-6">
+                                        <div class="card">
+                                          <div class="card-header">
+                                            <h4>Line Chart</h4>
+                                          </div>
+                                          <div class="card-body">
+                                            <canvas id="myChart"></canvas>
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="col-12 col-md-6 col-lg-6">
+                                        <div class="card">
+                                          <div class="card-header">
+                                            <h4>Bar Chart</h4>
+                                          </div>
+                                          <div class="card-body">
+                                            <canvas id="myChart2"></canvas>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div class="row">
+                                      <div class="col-12 col-md-6 col-lg-6">
+                                        <div class="card">
+                                          <div class="card-header">
+                                            <h4>Doughnut Chart</h4>
+                                          </div>
+                                          <div class="card-body">
+                                            <canvas id="myChart3"></canvas>
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="col-12 col-md-6 col-lg-6">
+                                        <div class="card">
+                                          <div class="card-header">
+                                            <h4>Pie Chart</h4>
+                                          </div>
+                                          <div class="card-body">
+                                            <canvas id="myChart4"></canvas>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                  59
-                </div>
-              </div>
+            </div>
         </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
+    </section>
+</div>
+
+@include('template.footer')
