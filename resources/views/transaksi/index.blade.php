e93696a9154f34641707cb3524f13f0cd19b0e61
kunkun <iqbalsetiawan536@gmail.com>
2024-11-29 16:47:15 +0700

    responsive

--- resources/views/transaksi/index.blade.php
+++ resources/views/transaksi/index.blade.php
@@ -2,126 +2,119 @@
 @include('template.navbar')
 @include('template.sidebar')
 <div class="main-content">
-    <section class="table">
-        {{-- <table>
-            <tr>
-                <th>timestamp</th>
-                <th>response_code</th>
-                <th>type_transaksi</th>
-                <th>url</th>
-                <th>response_message</th>
-            </tr>
-            @foreach ($data1 as $i)
-                
-            <tr>
-                <td>{{ $i->timestamp }}</td>
-                <td>{{ $i->response_code }}</td>
-                <td>{{ $i->type_transaksi }}</td>
-                <td>{{ $i->url }}</td>
-                <td>{{ $i->response_message }}</td>
-            </tr>
-            @endforeach
-        </table> --}}
-        <div class="">
+  <section class="container">
+      <div class="row">
+          <!-- First Chart Card -->
+          <div class="col-12 col-md-6 mb-4">
+              <div class="card shadow">
+                  <div class="card-body">
+                      <canvas id="responseChart" style="width: 100%; height: auto;"></canvas>
+                  </div>
+              </div>
+          </div>
 
-            <div class=" row">
-                <div class="card col-md-6">
-                    <canvas id="responseChart" class="card-body p-2 shadow" style="height: 25%"></canvas>
-                </div>
-                <div class="card  col-md-6">
-                    <canvas id="transaksi" class="card-body p-2 shadow" style="height: 25%"></canvas>
-                </div>
-            </div>
-        </div>
-    </section>
+          <!-- Second Chart Card -->
+          <div class="col-12 col-md-6 mb-4">
+              <div class="card shadow">
+                  <div class="card-body">
+                      <canvas id="transaksi" style="width: 100%; height: auto;"></canvas>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
 
-    <script>
-        const labels = @json($labels);
-        const counts = @json($counts);
+  <script>
+      const labels = @json($labels);
+      const counts = @json($counts);
 
-        const ctx11 = document.getElementById('responseChart').getContext('2d');
-        new Chart(ctx11, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Jumlah Transaksi',
-                    data: counts,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.7)',
-                        'rgba(75, 192, 192, 0.7)', 
-                        'rgba(255, 205, 86, 0.7)' 
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(255, 205, 86, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'top',
-                    },
-                    title: {
-                        display: true,
-                        text: 'Jumlah Transaksi per Response Code'
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-        
-    </script>
-    <script>
-        const labels1 = @json($labels1);
-        const counts1 = @json($counts1);
+      const ctx11 = document.getElementById('responseChart').getContext('2d');
+      new Chart(ctx11, {
+          type: 'bar',
+          data: {
+              labels: labels,
+              datasets: [{
+                  label: 'Jumlah Transaksi',
+                  data: counts,
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.7)',
+                      'rgba(75, 192, 192, 0.7)', 
+                      'rgba(255, 205, 86, 0.7)' 
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(255, 205, 86, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              responsive: true,
+              maintainAspectRatio: false,
+              plugins: {
+                  legend: {
+                      position: 'top',
+                  },
+                  title: {
+                      display: true,
+                      text: 'Jumlah Transaksi per Response Code'
+                  }
+              },
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
+      
+  </script>
+  <script>
+      const labels1 = @json($labels1);
+      const counts1 = @json($counts1);
 
-        const ctx = document.getElementById('transaksi').getContext('2d');
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels1,
-                datasets: [{
-                    label: 'jenis Transaksi',
-                    data: counts1,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.7)',
-                        'rgba(75, 192, 192, 0.7)', 
-                        'rgba(255, 205, 86, 0.7)' 
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(255, 205, 86, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                plugins: {
-                    legend: {
-                        position: 'top',
-                    },
-                    title: {
-                        display: true,
-                        text: 'Jumlah Transaksi per Response Code'
-                    }
-                },
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
+      const ctx = document.getElementById('transaksi').getContext('2d');
+      new Chart(ctx, {
+          type: 'bar',
+          data: {
+              labels: labels1,
+              datasets: [{
+                  label: 'Jenis Transaksi',
+                  data: counts1,
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.7)',
+                      'rgba(75, 192, 192, 0.7)', 
+                      'rgba(255, 205, 86, 0.7)' 
+                  ],
+                  borderColor: [
+                      'rgba(255, 99, 132, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(255, 205, 86, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              responsive: true,
+              maintainAspectRatio: false,
+              plugins: {
+                  legend: {
+                      position: 'top',
+                  },
+                  title: {
+                      display: true,
+                      text: 'Jumlah Transaksi per Jenis'
+                  }
+              },
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
+  </script>
+</div>
+
 @include('template.footer')
